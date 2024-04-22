a78a8dc055e2c6ec36568fcf71193ce85d67b41e
ClaroHenrique <clarohenriques@gmail.com>
2024-04-22 11:35:38 -0300

    Add JLD2 data files

--- fluxjl/_main.jl
+++ fluxjl/_main.jl
@@ -1,11 +1,7 @@
-# using Pkg; Pkg.respect_sysimage_versions(false)
-# ] dev /home/henrique/Documents/teste/Distributed.jl
-
 using ProgressMeter
 using Distributed
 using Profile
 include("src/utils.jl")
-#include("/home/henrique/Documents/teste/Distributed.jl")
 
 # instantiate and precompile environment in all processes
 @everywhere begin
@@ -19,54 +15,59 @@ end
   using Flux: train!, onehotbatch
   include("src/dataset_loader.jl")
   include("src/aggregate.jl")
+  include("src/train_parameter_server.jl")
+  include("src/train_client.jl")
 
-  # Define variables
+  # Define global constants
   n_servers = 2
   n_clients_per_server = 2
   n_clients = n_servers * n_clients_per_server
 
-  main_id = 1:1
-  server_ids = 2:1+n_servers
-  client_ids = 1+n_servers+1:1+n_servers+1+n_clients
-
-  main_worker_pool = WorkerPool(main_id)
-  server_worker_pool = WorkerPool(server_ids)
-  client_worker_pool = WorkerPool(client_ids)
-
-  if myid() == 1
-    println("WORKERS POOLS IDS")
-    println("main_id: $(main_id)")
-    println("server_ids: $(server_ids)")
-    println("client_ids: $(client_ids)")
-  end
-
-  num_epochs = 3
+  num_epochs = 20
   learning_rate = 0.001
-  batch_size = 64
+  batch_size = 32
   batchs_per_epoch = 100
   random_seed = 42
 
-  #define train functions
-  include("src/train_parameter_server.jl")
-  include("src/train_client.jl")
+  if myid() == 1
+    # Load test dataset
+    x_test, y_test = MLDatasets.CIFAR10.testdata()
+
+    # Create federated train dataset file, if it is not present  
+    try
+        JLD2.load("dataset/train_data_$(n_clients)_b$(batch_size).jld2", "ok")
+    catch err
+        if isa(err, ArgumentError)
+            initializate_fed_dataset(n_clients=n_clients, batch_size=batch_size)
+        else
+            throw(e)
+        end
+    end
+  end
 end
 
-println("Defining model")
-include("src/model_custom.jl")
+
+# Define model
+include("src/models/custom.jl")
 global_model = custom_model
 
-println("Testing model")
-println("Epoch: $(0), Accuracy: $(test_accuracy(global_model))")
+function log_model_accuracy(model, x, y; epoch)
+  println("Epoch: $(epoch), Model accuracy: $(test_accuracy(model, x, y))")
+end
 
+# Log model initial test accuracy
+log_model_accuracy(global_model, x_test, y_test; epoch=0)
 
+# Begin master node training
 @profile @showprogress for ep in 1:num_epochs
   global global_model
 
-  server_models = pmap(server_worker_pool, 1:n_servers, role=:default) do i
+  # Call the training from the parameter servers 
+  server_models = pmap(1:n_servers, role=:default) do i
     train_parameter_server(global_model, i)
   end
 
+  # Aggregate parameter servers' results
   global_model = aggregate(server_models)
-  println("Epoch: ", ep, ", Test accuracy: ", test_accuracy(global_model))
+  log_model_accuracy(global_model, x_test, y_test; epoch=ep)
 end
-
