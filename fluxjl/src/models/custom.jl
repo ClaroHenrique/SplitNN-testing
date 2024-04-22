291c309f8a391a3e3a4bc7a70d0949cf802c7dbf
ClaroHenrique <clarohenriques@gmail.com>
2024-04-22 11:35:06 -0300

    Move models to own folder

--- fluxjl/src/models/custom.jl
+++ fluxjl/src/models/custom.jl
@@ -9,10 +9,11 @@ custom_model = Chain(
   Conv((3, 3), 64 => 64, relu, pad=1),  # 147_584 parameters
   MaxPool((2, 2), pad=1),
   MLUtils.flatten,
-  Dense(5184 => 512, relu),            # 5_308_928 parameters
+  Dense(5184 => 512, relu),             # 5_308_928 parameters
   Dropout(0.5),
   Dense(512 => 256, relu),              # 131_328 parameters
   Dropout(0.5),
   Dense(256 => 10),                     # 2_570 parameters
-) # Total: 14 arrays, 5_702_986 parameters, 21.757 MiB.
+)
+# Total: 14 arrays, 5_702_986 parameters, 21.757 MiB.
 
