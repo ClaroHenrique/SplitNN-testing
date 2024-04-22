5fb8d6f0c89a9d631d9ef89d849a9353e2b45440
ClaroHenrique <clarohenriques@gmail.com>
2024-04-22 11:36:03 -0300

    Refactor

--- fluxjl/src/train_client.jl
+++ fluxjl/src/train_client.jl
@@ -2,27 +2,30 @@ include("aggregate.jl")
 using Flux
 
 function train_client(model, client_id)
-  println("Client $(client_id) (Process(m, w): $(myid(role=:master)), $(myid(role=:worker))) is training")
-  data_loader = dataset_loader(id=client_id, np=n_clients, batch_size=batch_size, random_seed=random_seed)
-  sz = length(data_loader)
+  # Load federated dataset
+  data_loader = dataset_loader(id=client_id, n_clients=n_clients, batch_size=batch_size)
 
+  # Initializate optimizer
   optimizer = Flux.setup(Flux.Adam(learning_rate), model)
-  num_batches = length(data_loader)
-  loss = 0.
 
+  # Run SGD
   for (i, (x, y)) in enumerate(data_loader)
-    l, (local_grad, ) = Flux.withgradient(model, x) do model, x
+    # Calculate grads
+    loss, (local_grad, ) = Flux.withgradient(model, x) do model, x
       y_hat = model(x)
       Flux.logitcrossentropy(y_hat, y)
     end
-    # println("Progress: $(i)/$(sz) or $(i/sz*100)%")
-    loss += l / num_batches
+    
+    # Use grads to update the local model
     Flux.update!(optimizer, model, local_grad)
+    
+    # Stops if reached limit of batchs per epoch
     if(i == batchs_per_epoch)
       break
     end
   end
 
-  # println("Loss: ", loss)
+  # Return the updated local model, i.i.e the initial
+  # copy of the global model trained with local data
   model
 end
--- fluxjl/src/train_parameter_server.jl
+++ fluxjl/src/train_parameter_server.jl
@@ -1,14 +1,13 @@
 include("aggregate.jl")
 
 function train_parameter_server(global_model, server_id)
-  println("Training server $(server_id) (Process: $(myid())).")
-
-  local_models = pmap(client_worker_pool, 1:n_clients_per_server, role=:default) do i
+  # Call the training from the clients 
+  local_models = pmap(1:n_clients_per_server, role=:default) do i
     client_id = (server_id-1) * n_servers + i
     train_client(global_model, client_id)
   end
 
-  #println("Epoch: ", ep, ", Test accuracy: ", test_accuracy(global_model))
+  # Aggregate parameter clients' results
   server_model = aggregate(local_models)
   server_model
 end
