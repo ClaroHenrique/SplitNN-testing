5630eed5fd19cac834eceb9a3b676d226f33d433
ClaroHenrique <clarohenriques@gmail.com>
2024-05-28 11:21:41 -0300

    Add single node version

---
+++ cloudclustersjl_studycase/_main_single_node.jl
@@ -0,0 +1,41 @@
+using ProgressMeter
+using Profile
+using Flux, MLDatasets
+using Flux: train!, onehotbatch
+using Dates
+
+include("src/utils.jl")
+include("src/aggregate.jl")
+include("src/models/custom.jl")
+include("src/train_client.jl")
+include("src/dataset_loader.jl")
+
+### Inicitalizate client nodes ###
+
+println("Initializating clients")
+
+# Define constants
+learning_rate = 0.001
+batch_size = 32
+iterations_per_client = 100
+data_loader = dataset_loader(batch_size)
+
+# Define model
+model = custom_model
+
+# include("src/models/vgg16.jl")
+# model = vgg16
+
+# Log model initial test accuracy
+initial_timestamp = now()
+log_test_accuracy(model; epoch=0, timestamp=now() - initial_timestamp)
+
+# Begin distributed training
+println("Start training")
+num_epochs = 20
+
+@profile @showprogress for ep in 1:num_epochs
+  global model
+  train_client(model)
+  log_test_accuracy(model; epoch=ep, timestamp=now()-initial_timestamp)
+end
