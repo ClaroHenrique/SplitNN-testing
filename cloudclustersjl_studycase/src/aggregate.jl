52d4f95874f76927607c0b8677715b119b0a3d7a
Francisco Heron de Carvalho Junior <heron@dc.ufc.br>
2024-05-29 10:02:36 -0300

    aggregate gpu

--- cloudclustersjl_studycase/src/aggregate.jl
+++ cloudclustersjl_studycase/src/aggregate.jl
@@ -8,25 +8,20 @@ function aggregate(models::AbstractVector{T}) where T <: Chain
   for (i, layer) in enumerate(models[1])
     if layer isa Dense || layer isa Conv
       params = Flux.params(layer)
-      new_layer_w = CUDA.zeros(size(params[1])) #zeros(Float32, size(params[1])) 
-      new_layer_b = CUDA.zeros(size(params[2])) #zeros(Float32, size(params[2]))
+      new_layer_w = zeros(Float32, size(params[1]))
+      new_layer_b = zeros(Float32, size(params[2]))
 
       # sum current layer's params from all the models 
       for m in models
         params = Flux.params(m[i])
-        params_1 = cu(params[1])
-        params_2 = cu(params[2])
-        new_layer_w += params_1 
-        new_layer_b += params_2 
+        new_layer_w += params[1]
+        new_layer_b += params[2]
       end
 
       # get mean from the division
       new_layer_w ./= n_models
       new_layer_b ./= n_models
       
-      new_layer_w = new_layer_w |> cpu
-      new_layer_b = new_layer_b |> cpu
-
       if layer isa Dense
         push!(new_model_layers, Dense(new_layer_w, new_layer_b, layer.σ))
       else # layer isa Conv
