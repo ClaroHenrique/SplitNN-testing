ba53e589ff534bd3f0a6b2d2f666b1de1aec7b0c
clarohenrique <clarohenriques@gmail.com>
2024-06-01 17:51:41 -0300

    Move model output to cpu

--- cloudclustersjl_studycase/src/utils.jl
+++ cloudclustersjl_studycase/src/utils.jl
@@ -1,10 +1,13 @@
+using CUDA
+
 function test_accuracy(model, data_loader)
     correct = 0
     total = 0
     for (x, y) in data_loader
         n = size(x)[end]
-        y_pred = reshape(map(x -> x[1][1] -1, findmax(model(x), dims=1)[2]), n)
-        y_true = reshape(map(x -> x[1][1] -1, findmax(y       , dims=1)[2]), n)
+        output = model(x) |> cpu
+        y_pred = reshape(map(x -> x[1][1] -1, findmax(output, dims=1)[2]), n)
+        y_true = reshape(map(x -> x[1][1] -1, findmax(y     , dims=1)[2]), n)
         correct += sum(y_pred .== y_true)
         total += n
     end
